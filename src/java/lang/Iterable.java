412dfd8f6c162d8a6e6af6fdc128729b88a44dec
If <ifhuang91@gmail.com>
2014-10-07 15:22:37 +0800

    Iterable, Iterator, Collection.

--- src/java/lang/Iterable.java
+++ src/java/lang/Iterable.java
@@ -1,26 +1,6 @@
 /*
  * Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
 
 package java.lang;
@@ -28,19 +8,21 @@ package java.lang;
 import java.util.Iterator;
 
 /**
- * Implementing this interface allows an object to be the target of
- * the "foreach" statement.
+ * Implementing this interface allows an object to be the target of the
+ * "foreach" statement.
  *
- * @param <T> the type of elements returned by the iterator
+ * @param <T>
+ *            the type of elements returned by the iterator
  *
  * @since 1.5
  */
-public interface Iterable<T> {
+public interface Iterable<T>
+{
 
-    /**
-     * Returns an iterator over a set of elements of type T.
-     *
-     * @return an Iterator.
-     */
-    Iterator<T> iterator();
+	/**
+	 * Returns an iterator over a set of elements of type T.
+	 *
+	 * @return an Iterator.
+	 */
+	Iterator<T> iterator();
 }
