34bff7a9649344b70988d8d8b69bad4fe2efe043
If <ifhuang91@gmail.com>
2014-10-26 01:42:59 +0800

    Object, Override, Package, Process, Runnable, Runtime, String, CharSequence, StringBuffer, AbstractStringBuilder, Appendable, StringBuilder, System, Thread, Void

--- src/java/lang/Void.java
+++ src/java/lang/Void.java
@@ -1,49 +1,30 @@
 /*
  * Copyright (c) 1996, 2006, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
 
 package java.lang;
 
 /**
  * The {@code Void} class is an uninstantiable placeholder class to hold a
- * reference to the {@code Class} object representing the Java keyword
- * void.
+ * reference to the {@code Class} object representing the Java keyword void.
  *
- * @author  unascribed
- * @since   JDK1.1
+ * @author unascribed
+ * @since JDK1.1
  */
-public final
-class Void {
+public final class Void
+{
 
-    /**
-     * The {@code Class} object representing the pseudo-type corresponding to
-     * the keyword {@code void}.
-     */
-    public static final Class<Void> TYPE = Class.getPrimitiveClass("void");
+	/**
+	 * The {@code Class} object representing the pseudo-type corresponding to
+	 * the keyword {@code void}.
+	 */
+	public static final Class<Void> TYPE = Class.getPrimitiveClass("void");
 
-    /*
-     * The Void class cannot be instantiated.
-     */
-    private Void() {}
+	/*
+	 * The Void class cannot be instantiated.
+	 */
+	private Void()
+	{
+	}
 }
