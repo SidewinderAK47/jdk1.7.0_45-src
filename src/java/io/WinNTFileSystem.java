8eb378fde15d9587e26a4abfb779b5880c45ca0b
Yifu Huang <ifhuang91@gmail.com>
2015-06-20 01:51:14 +0800

    java.io 2

--- src/java/io/WinNTFileSystem.java
+++ src/java/io/WinNTFileSystem.java
@@ -1,26 +1,6 @@
 /*
- * Copyright (c) 2001, 2006, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ * Copyright (c) 2001, 2006, Oracle and/or its affiliates. All rights reserved. ORACLE
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
 /*
@@ -36,44 +16,54 @@ package java.io;
  */
 class WinNTFileSystem extends Win32FileSystem {
 
-    protected native String canonicalize0(String path)
-                                                throws IOException;
-    protected native String canonicalizeWithPrefix0(String canonicalPrefix,
-                                                    String pathWithCanonicalPrefix)
-                                                throws IOException;
+  protected native String canonicalize0(String path) throws IOException;
 
-    /* -- Attribute accessors -- */
+  protected native String canonicalizeWithPrefix0(String canonicalPrefix,
+      String pathWithCanonicalPrefix) throws IOException;
 
-    public native int getBooleanAttributes(File f);
-    public native boolean checkAccess(File f, int access);
-    public native long getLastModifiedTime(File f);
-    public native long getLength(File f);
-    public native boolean setPermission(File f, int access, boolean enable, boolean owneronly);
+  /* -- Attribute accessors -- */
+
+  public native int getBooleanAttributes(File f);
+
+  public native boolean checkAccess(File f, int access);
+
+  public native long getLastModifiedTime(File f);
+
+  public native long getLength(File f);
+
+  public native boolean setPermission(File f, int access, boolean enable, boolean owneronly);
 
 
-    public long getSpace(File f, int t) {
-        if (f.exists()) {
-            return getSpace0(f, t);
-        }
-        return 0;
+  public long getSpace(File f, int t) {
+    if (f.exists()) {
+      return getSpace0(f, t);
     }
+    return 0;
+  }
 
-    private native long getSpace0(File f, int t);
+  private native long getSpace0(File f, int t);
 
-    /* -- File operations -- */
+  /* -- File operations -- */
 
-    public native boolean createFileExclusively(String path)
-                                               throws IOException;
-    protected native boolean delete0(File f);
-    public native String[] list(File f);
-    public native boolean createDirectory(File f);
-    protected native boolean rename0(File f1, File f2);
-    public native boolean setLastModifiedTime(File f, long time);
-    public native boolean setReadOnly(File f);
-    protected native String getDriveDirectory(int drive);
-    private static native void initIDs();
+  public native boolean createFileExclusively(String path) throws IOException;
 
-    static {
-            initIDs();
-    }
+  protected native boolean delete0(File f);
+
+  public native String[] list(File f);
+
+  public native boolean createDirectory(File f);
+
+  protected native boolean rename0(File f1, File f2);
+
+  public native boolean setLastModifiedTime(File f, long time);
+
+  public native boolean setReadOnly(File f);
+
+  protected native String getDriveDirectory(int drive);
+
+  private static native void initIDs();
+
+  static {
+    initIDs();
+  }
 }
--- src/java/nio/channels/ByteChannel.java
+++ src/java/nio/channels/ByteChannel.java
@@ -1,26 +1,6 @@
 /*
- * Copyright (c) 2000, 2001, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ * Copyright (c) 2000, 2001, Oracle and/or its affiliates. All rights reserved. ORACLE
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
 package java.nio.channels;
@@ -29,17 +9,15 @@ import java.io.IOException;
 
 
 /**
- * A channel that can read and write bytes.  This interface simply unifies
- * {@link ReadableByteChannel} and {@link WritableByteChannel}; it does not
- * specify any new operations.
+ * A channel that can read and write bytes. This interface simply unifies
+ * {@link ReadableByteChannel} and {@link WritableByteChannel}; it does not specify any new
+ * operations.
  *
  * @author Mark Reinhold
  * @author JSR-51 Expert Group
  * @since 1.4
  */
 
-public interface ByteChannel
-    extends ReadableByteChannel, WritableByteChannel
-{
+public interface ByteChannel extends ReadableByteChannel, WritableByteChannel {
 
 }
