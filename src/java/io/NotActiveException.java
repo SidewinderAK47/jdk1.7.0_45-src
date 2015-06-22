5c6854d424574422775a8d29cb80fd33d02465e0
Yifu Huang <ifhuang91@gmail.com>
2015-06-23 01:26:06 +0800

    java.io 3

--- src/java/io/NotActiveException.java
+++ src/java/io/NotActiveException.java
@@ -1,26 +1,6 @@
 /*
- * Copyright (c) 1996, 2005, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ * Copyright (c) 1996, 2005, Oracle and/or its affiliates. All rights reserved. ORACLE
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
 package java.io;
@@ -28,26 +8,26 @@ package java.io;
 /**
  * Thrown when serialization or deserialization is not active.
  *
- * @author  unascribed
- * @since   JDK1.1
+ * @author unascribed
+ * @since JDK1.1
  */
 public class NotActiveException extends ObjectStreamException {
 
-    private static final long serialVersionUID = -3893467273049808895L;
+  private static final long serialVersionUID = -3893467273049808895L;
 
-    /**
-     * Constructor to create a new NotActiveException with the reason given.
-     *
-     * @param reason  a String describing the reason for the exception.
-     */
-    public NotActiveException(String reason) {
-        super(reason);
-    }
+  /**
+   * Constructor to create a new NotActiveException with the reason given.
+   *
+   * @param reason a String describing the reason for the exception.
+   */
+  public NotActiveException(String reason) {
+    super(reason);
+  }
 
-    /**
-     * Constructor to create a new NotActiveException without a reason.
-     */
-    public NotActiveException() {
-        super();
-    }
+  /**
+   * Constructor to create a new NotActiveException without a reason.
+   */
+  public NotActiveException() {
+    super();
+  }
 }
